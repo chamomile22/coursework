ae5c204b0d6205c097c4ef201ce2144e2bfcce9c
Roman Hranovskyi <bartaimpson1@gmail.com>
2024-05-31 00:11:46 +0300

    feat: services with department

--- server-coursework/src/modules/services/services/services.service.ts
+++ server-coursework/src/modules/services/services/services.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { FilterQuery, Model, Types } from "mongoose";
+import { FilterQuery, Model, PipelineStage, Types } from "mongoose";
+import { ServiceException } from "@module/common/exceptions";
 import { CreateServiceDto, GetAllServicesDto, UpdateServiceDto } from "../dto";
 import { ServiceDocument, ServiceEntity } from "../schemas";
 
@@ -8,18 +9,35 @@ import { ServiceDocument, ServiceEntity } from "../schemas";
 export class ServicesService {
   constructor(
     @InjectModel(ServiceEntity.name)
-    private readonly equipmentModel: Model<ServiceEntity>,
+    private readonly serviceModel: Model<ServiceEntity>,
   ) {}
+
+  private configureDefaultLookupOptions(): PipelineStage[] {
+    return [
+      {
+        $lookup: {
+          from: "departments",
+          localField: "departmentId",
+          foreignField: "_id",
+          as: "department",
+        },
+      },
+      { $addFields: { "department": "$department.name" } },
+      {
+        $unwind: { path: "$department" },
+      },
+    ];
+  }
 
   async create(data: CreateServiceDto): Promise<ServiceDocument> {
     const { equipmentIds, ...restParams } = data;
 
     if (equipmentIds) {
       const ids = equipmentIds.map(item => new Types.ObjectId(item));
-      return await this.equipmentModel.create({ equipmentIds: ids, ...restParams });
+      return await this.serviceModel.create({ equipmentIds: ids, ...restParams });
     }
 
-    return await this.equipmentModel.create(data);
+    return await this.serviceModel.create(data);
   }
 
   async getAll(params: GetAllServicesDto): Promise<ServiceDocument[]> {
@@ -38,32 +56,49 @@ export class ServicesService {
       }
     }
 
-    return await this.equipmentModel
-      .find(where)
-      .sort(sort);
+    const additionalPipelines: PipelineStage[] = [{ $match: where }];
+
+    if (sort) {
+      additionalPipelines.push({ $sort: sort });
+    }
+
+    return await this.serviceModel.aggregate<ServiceDocument>([
+      ...additionalPipelines,
+      ...this.configureDefaultLookupOptions(),
+    ]);
   }
+
 
   async updateById(id: string, data: UpdateServiceDto): Promise<ServiceDocument> {
     const { equipmentIds, ...restParams } = data;
 
     if (equipmentIds) {
       const ids = equipmentIds.map(item => new Types.ObjectId(item));
-      return await this.equipmentModel.findByIdAndUpdate(id, { equipmentIds: ids, ...restParams }, { new: true });
+      return await this.serviceModel.findByIdAndUpdate(id, { equipmentIds: ids, ...restParams }, { new: true });
     }
 
-    return await this.equipmentModel.findByIdAndUpdate(id, data, { new: true });
+    return await this.serviceModel.findByIdAndUpdate(id, data, { new: true });
   }
 
   async findById(id: string): Promise<ServiceDocument> {
-    return await this.equipmentModel.findById(id);
+    const result = await this.serviceModel.aggregate<ServiceDocument>([
+      { $match: { _id: new Types.ObjectId(id) } },
+      ...this.configureDefaultLookupOptions(),
+    ]);
+
+    if (!result?.length) {
+      throw new ServiceException("Service was not found");
+    }
+
+    return result[0];
   }
 
   async findOne(data: FilterQuery<ServiceEntity>): Promise<ServiceDocument> {
-    return await this.equipmentModel.findOne(data);
+    return await this.serviceModel.findOne(data);
   }
 
   async deleteById(id: string): Promise<boolean> {
-    const result = await this.equipmentModel.deleteOne({ _id: id });
+    const result = await this.serviceModel.deleteOne({ _id: id });
     return result.deletedCount !== 0;
   }
 }
